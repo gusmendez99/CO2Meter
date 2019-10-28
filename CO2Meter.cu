49936d75d4bf968889328222a11bdabec6489458
Gus Mendez <gus.mendez.99@gmail.com>
2019-10-28 08:19:54 -0600

    Updated, using 2 streams

--- CO2Meter.cu
+++ CO2Meter.cu
@@ -36,10 +36,78 @@ using namespace std;
 #define PRESSURE_OUTDOOR (852.0f)  //Pressure outside
 #define TEMP_OUTDOOR (24.1f)     //Temperature outside 
 
-/* Function to load data from CSV file */
-void loadGasParams (float *a, float *b, float *c, int n)
+/* CO2 model check function */
+__global__ void getGasModelKernel1(float *a1, float *b1, float *c1, float *d1)
 {
-    //Loading CSV
+    float currVolume = A304_VOLUME;
+    float currPersons = A304_PERSONS;
+
+    int index = threadIdx.x + blockDim.x * blockIdx.x;				
+	if (index < N)
+	{
+		d1[index] = (b1[index] / (c1[index] * (1 + ACH * (index/10)))) * ( ((a1[0] * c1[0])/b1[0]) + ( (((CO2_ADULT_GAIN * currPersons * R)/(CO2_MASS * currVolume)) + ((CO2_OUTDOOR * PRESSURE_OUTDOOR * ACH)/(TEMP_OUTDOOR))) * (index/10)));
+	}
+    
+}
+
+/* CO2 model check function */
+__global__ void getGasModelKernel2(float *a2, float *b2, float *c2, float *d2)
+{    
+    //AVG a[index]
+    float currVolume = C114_VOLUME;
+    float currPersons = C114_PERSONS;
+
+    int index = threadIdx.x + blockDim.x * blockIdx.x;				
+	if (index < N)
+	{
+		d2[index] = (b2[index] / (c2[index] * (1 + ACH * (index/10)))) * ( ((a2[0] * c2[0])/b2[0]) + ( (((CO2_ADULT_GAIN * currPersons * R)/(CO2_MASS * currVolume)) + ((CO2_OUTDOOR * PRESSURE_OUTDOOR * ACH)/(TEMP_OUTDOOR))) * (index/10)));
+	}
+    
+}
+
+int main(void)
+{
+    /* Vectors contains:
+        - a: real CO2 value, in PPM
+        - b: temperature value, in Celsius
+        - c: pressure value, in hPa
+        - e: CO2 model value, in PPM
+    */
+
+    cudaStream_t stream1, stream2;
+	cudaStreamCreate(&stream1);
+	cudaStreamCreate(&stream2);
+    
+    float *a1, *b1, *c1, *d1, *a2, *b2, *c2, *d2; //host copies of a,b,c vectors
+    float *d_a1, *d_b1, *d_c1, *d_d1, *d_a2, *d_b2, *d_c2, *d_d2; //device copies of a,b,c etc
+
+    int size = N * sizeof(float); //size of memory that needs to be allocated
+
+    //Allocate space for device copies of a,b,c, e
+    cudaMalloc((void **)&d_a1, size);
+    cudaMalloc((void **)&d_b1, size);
+    cudaMalloc((void **)&d_c1, size);
+    cudaMalloc((void **)&d_d1, size);
+    cudaMalloc((void **)&d_a2, size);
+    cudaMalloc((void **)&d_b2, size);
+    cudaMalloc((void **)&d_c2, size);
+    cudaMalloc((void **)&d_d2, size);
+
+    //Allows device to get access to memory
+    cudaHostAlloc( (void**)&a1, size, cudaHostAllocDefault);	 
+    cudaHostAlloc( (void**)&b1, size, cudaHostAllocDefault);
+    cudaHostAlloc( (void**)&c1, size, cudaHostAllocDefault);
+    cudaHostAlloc( (void**)&d1, size, cudaHostAllocDefault);
+    cudaHostAlloc( (void**)&a2, size, cudaHostAllocDefault);	 
+    cudaHostAlloc( (void**)&b2, size, cudaHostAllocDefault);
+    cudaHostAlloc( (void**)&c2, size, cudaHostAllocDefault);
+    cudaHostAlloc( (void**)&d2, size, cudaHostAllocDefault);
+
+    /*Loading CSV
+        Room Structure:
+            - A304: 1 - 41500
+            - C114: 41501 - 83000 
+    */
 	ifstream ip("finalData.csv");
 
 	if (!ip.is_open())
@@ -61,102 +129,78 @@ void loadGasParams (float *a, float *b, float *c, int n)
 		double tempDouble = ::atof(temperatureString.c_str());
         double pressureDouble = ::atof(pressureString.c_str());
 
-        a[i] = float(gasDouble);
-        b[i] = float(tempDouble);
-        c[i] = float(pressureDouble);
+        if(N/2 < 41500) {
+            a1[i] = float(gasDouble);
+            b1[i] = float(tempDouble);
+            c1[i] = float(pressureDouble);
+        } else {
+            a2[i] = float(gasDouble);
+            b2[i] = float(tempDouble);
+            c2[i] = float(pressureDouble);
+        }
 
         i++;
 				
     }
     
     printf("# OF DATA: %d \n", i - 1);
-
 	ip.close();
-    return;
-}
-
-/* CO2 model check function */
-__global__ void gasMeterModelCalc(float *a, float *b, float *c, float *e)
-{
     
-    //AVG a[index]
-    float currVolume = (N/2 > 41500) ? C114_VOLUME : A304_VOLUME;
-    float currPersons = (N/2 > 41500) ? C114_PERSONS : A304_PERSONS;
-
-    int index = threadIdx.x + blockDim.x * blockIdx.x;				
-	if (index < N)
-	{
-		e[index] = (b[index] / (c[index] * (1 + ACH * (index/10)))) * ( ((a[0] * c[0])/b[0]) + ( (((CO2_ADULT_GAIN * currPersons * R)/(CO2_MASS * currVolume)) + ((CO2_OUTDOOR * PRESSURE_OUTDOOR * ACH)/(TEMP_OUTDOOR))) * (index/10)));
-	}
-    
-}
-
-int main(int argc, char** argv)
-{
-    /* Vectors contains:
-        - a: real CO2 value, in PPM
-        - b: temperature value, in Celsius
-        - c: pressure value, in hPa
-        - e: CO2 model value, in PPM
-    */
-
-    cudaStream_t myStream;
-    cudaStreamCreate(&myStream);
-    
-    float *a, *b, *c, *e; //host copies of a,b,c vectors
-    float *d_a, *d_b, *d_c, *d_e; //device copies of a,b,c etc
-
-    int size = N * sizeof(float); //size of memory that needs to be allocated
-
-    //Allocate space for device copies of a,b,c, e
-    cudaMalloc((void **)&d_a, size);
-    cudaMalloc((void **)&d_b, size);
-    cudaMalloc((void **)&d_c, size);
-    cudaMalloc((void **)&d_e, size);
-
-    //Allows device to get access to memory
-    cudaHostAlloc( (void**)&a, N * sizeof(int), cudaHostAllocDefault);	 
-    cudaHostAlloc( (void**)&b, N * sizeof(int), cudaHostAllocDefault);
-    cudaHostAlloc( (void**)&c, N * sizeof(int), cudaHostAllocDefault);
-    cudaHostAlloc( (void**)&e, N * sizeof(int), cudaHostAllocDefault);
-
-    //loading arrays
-    loadGasParams(a, b, c, N);
 
     //Timing
     struct timeval t1, t2;
     gettimeofday(&t1, 0);
 
-    //Using streams
-    for(int i=0; i < N; i+= N*2) { 
-        // using stream 1 and steam 2
-        cudaMemcpyAsync(d_a, a, N*sizeof(int), cudaMemcpyHostToDevice, myStream);
-        cudaMemcpyAsync(d_b, b, N*sizeof(int), cudaMemcpyHostToDevice, myStream);
-        cudaMemcpyAsync(d_c, c, N*sizeof(int), cudaMemcpyHostToDevice, myStream);
-        
-        
-        gasMeterModelCalc<<<(int)ceil(N/1024)+1, 1024, 0, myStream>>>(d_a, d_b, d_c, d_e);
-        cudaMemcpyAsync(e, d_e, N*sizeof(int), cudaMemcpyDeviceToHost, myStream);
+    for(int i=0;i < N;i++)
+	{   // loop over data in chunks
+	    // stream 1
+		cudaMemcpyAsync(d_a1, a1, size, cudaMemcpyHostToDevice, stream1);
+        cudaMemcpyAsync(d_b1, b1, size, cudaMemcpyHostToDevice, stream1);
+        cudaMemcpyAsync(d_c1, c1, size, cudaMemcpyHostToDevice, stream1);
+		getGasModelKernel1<<<1,N,0,stream1>>>(d_a1, d_b1, d_c1, d_d1);
+		cudaMemcpyAsync(d1, d_d1, size, cudaMemcpyDeviceToHost, stream1);
+
+		//stream 2
+		cudaMemcpyAsync(d_a2, a2, size, cudaMemcpyHostToDevice, stream2);
+		cudaMemcpyAsync(d_b2, b2, size, cudaMemcpyHostToDevice, stream2);
+        cudaMemcpyAsync(d_c2, c2, size, cudaMemcpyHostToDevice, stream2);
+        getGasModelKernel2<<<1,N,1,stream2>>>(d_a2, d_b2, d_c2, d_d2);
+		cudaMemcpyAsync(d2, d_c2, size, cudaMemcpyDeviceToHost, stream2);
     }
+    
+	cudaStreamSynchronize(stream1); // wait for stream1 to finish
+	cudaStreamSynchronize(stream2); // wait for stream2 to finish
 
     gettimeofday(&t2, 0);
     float time = (1000000.0*(t2.tv_sec-t1.tv_sec) + t2.tv_usec-t1.tv_usec)/1000.0;
     printf("EXECUTION TIME:  %5.4f ms \n", time);
 
-
-    int i;
+    //A304
     //Some values of real CO2 from 3000 to 3010
-    printf("MODEL CO2 = [");
-    for (i=25000; i<25010; i++) printf(" %4.3f", e[i]);
+    printf("MODEL CO2 - A304 = [");
+    for (i=25000; i<25010; i++) printf(" %4.3f", d1[i]);
     printf(" ...]\n");
 
     //Some values of real CO2 from 3000 to 3010
-    printf("REAL CO2 = [");
-    for (i=25000; i<25010; i++) printf(" %4.3f", a[i]);
+    printf("REAL CO2 - A304= [");
+    for (i=25000; i<25010; i++) printf(" %4.3f", a1[i]);
     printf(" ...]\n");
-
     
+    
+    //C114
+    //Some values of real CO2 from 3000 to 3010
+    printf("MODEL CO2 - C114 = [");
+    for (i=25000; i<25010; i++) printf(" %4.3f", d2[i]);
+    printf(" ...]\n");
+
+    //Some values of real CO2 from 3000 to 3010
+    printf("REAL CO2 - C114 = [");
+    for (i=25000; i<25010; i++) printf(" %4.3f", a2[i]);
+    printf(" ...]\n");
+
+
     //Destroying stream used
-    cudaStreamDestroy(myStream);
+    cudaStreamDestroy(stream1);
+    cudaStreamDestroy(stream2);
 	return 0;
 } 
