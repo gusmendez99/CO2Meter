5f82e5c37a979ab6ca0f3f9c5f00f19b14cc24e8
Gus Mendez <gus.mendez.99@gmail.com>
2019-10-28 19:10:50 -0600

    Minor bugs found

--- CO2Meter.cu
+++ CO2Meter.cu
@@ -21,26 +21,26 @@ Description: Test of measurement model of CO2 in a closed environment, depending
 #include <fstream>
 using namespace std;
 
-#define N 83000                 //Threads, and length of arrays declared
+#define N 41500                         //Threads, and length of arrays declared
 
-#define R (8.314f)               //Gas constant
-#define ACH (3.5f)               //ACH for a classroom
-#define CO2_MASS (44.01f)        //CO2 Molar mass 
-#define CO2_ADULT_GAIN (0.0052f) //CO2 gain by an adult, in l/s
-#define A304_VOLUME (114.20f)    //m3
-#define C114_VOLUME (125.625f)   //m3
-#define A304_PERSONS (22.0f)       // no. persons in this classroom
-#define C114_PERSONS (16.0f)       // no. persons in this classroom
+const double R = 8.314;                 //Gas constant
+const double ACH = 3.5;                 //ACH for a classroom
+const double CO2_MASS = 44.01;            //CO2 Molar mass 
+const double CO2_ADULT_GAIN = 0.0052;   //CO2 gain by an adult, in l/s
+const double A304_VOLUME = 114.20;      //m3
+const double C114_VOLUME = 125.625;     //m3
+const double A304_PERSONS = 22.0;       // no. persons in this classroom
+const double C114_PERSONS = 16.0;       // no. persons in this classroom
 
-#define CO2_OUTDOOR (404.0f)       //CO2 outside, in PPM
-#define PRESSURE_OUTDOOR (852.0f)  //Pressure outside
-#define TEMP_OUTDOOR (24.1f)     //Temperature outside 
+const double CO2_OUTDOOR = 404.0;       //CO2 outside, in PPM
+const double PRESSURE_OUTDOOR = 852.0;  //Pressure outside
+const double TEMP_OUTDOOR = 24.1;       //Temperature outside 
 
 /* CO2 model check function */
-__global__ void getGasModelKernel1(float *a1, float *b1, float *c1, float *d1)
+__global__ void getGasModelKernel1(double *a1, double *b1, double *c1, double *d1)
 {
-    float currVolume = A304_VOLUME;
-    float currPersons = A304_PERSONS;
+    double currVolume = A304_VOLUME;
+    double currPersons = A304_PERSONS;
 
     int index = threadIdx.x + blockDim.x * blockIdx.x;				
 	if (index < N)
@@ -51,11 +51,11 @@ __global__ void getGasModelKernel1(float *a1, float *b1, float *c1, float *d1)
 }
 
 /* CO2 model check function */
-__global__ void getGasModelKernel2(float *a2, float *b2, float *c2, float *d2)
+__global__ void getGasModelKernel2(double *a2, double *b2, double *c2, double *d2)
 {    
     //AVG a[index]
-    float currVolume = C114_VOLUME;
-    float currPersons = C114_PERSONS;
+    double currVolume = C114_VOLUME;
+    double currPersons = C114_PERSONS;
 
     int index = threadIdx.x + blockDim.x * blockIdx.x;				
 	if (index < N)
@@ -65,7 +65,7 @@ __global__ void getGasModelKernel2(float *a2, float *b2, float *c2, float *d2)
     
 }
 
-int main(void)
+int main(int argc, char** argv)
 {
     /* Vectors contains:
         - a: real CO2 value, in PPM
@@ -78,16 +78,17 @@ int main(void)
 	cudaStreamCreate(&stream1);
 	cudaStreamCreate(&stream2);
     
-    float *a1, *b1, *c1, *d1, *a2, *b2, *c2, *d2; //host copies of a,b,c vectors
-    float *d_a1, *d_b1, *d_c1, *d_d1, *d_a2, *d_b2, *d_c2, *d_d2; //device copies of a,b,c etc
+    double *a1, *b1, *c1, *d1, *a2, *b2, *c2, *d2; //host copies of a,b,c vectors
+    double *d_a1, *d_b1, *d_c1, *d_d1, *d_a2, *d_b2, *d_c2, *d_d2; //device copies of a,b,c etc
 
-    int size = N * sizeof(float); //size of memory that needs to be allocated
+    int size = N * sizeof(double); //size of memory that needs to be allocated
 
     //Allocate space for device copies of a,b,c, e
     cudaMalloc((void **)&d_a1, size);
     cudaMalloc((void **)&d_b1, size);
     cudaMalloc((void **)&d_c1, size);
     cudaMalloc((void **)&d_d1, size);
+
     cudaMalloc((void **)&d_a2, size);
     cudaMalloc((void **)&d_b2, size);
     cudaMalloc((void **)&d_c2, size);
@@ -98,6 +99,7 @@ int main(void)
     cudaHostAlloc( (void**)&b1, size, cudaHostAllocDefault);
     cudaHostAlloc( (void**)&c1, size, cudaHostAllocDefault);
     cudaHostAlloc( (void**)&d1, size, cudaHostAllocDefault);
+    
     cudaHostAlloc( (void**)&a2, size, cudaHostAllocDefault);	 
     cudaHostAlloc( (void**)&b2, size, cudaHostAllocDefault);
     cudaHostAlloc( (void**)&c2, size, cudaHostAllocDefault);
@@ -108,6 +110,8 @@ int main(void)
             - A304: 1 - 41500
             - C114: 41501 - 83000 
     */
+
+
 	ifstream ip("finalData.csv");
 
 	if (!ip.is_open())
@@ -124,19 +128,14 @@ int main(void)
 		getline(ip, temperatureString, ',');
 		getline(ip, pressureString, '\n');
 
-        //Cast string to float, and saving values in array
-		double gasDouble = ::atof(gasString.c_str());
-		double tempDouble = ::atof(temperatureString.c_str());
-        double pressureDouble = ::atof(pressureString.c_str());
-
-        if(N/2 < 41500) {
-            a1[i] = float(gasDouble);
-            b1[i] = float(tempDouble);
-            c1[i] = float(pressureDouble);
+        if(i < N) {
+            a1[i] = ::atof(gasString.c_str());
+            b1[i] = ::atof(temperatureString.c_str());
+            c1[i] = ::atof(pressureString.c_str());
         } else {
-            a2[i] = float(gasDouble);
-            b2[i] = float(tempDouble);
-            c2[i] = float(pressureDouble);
+            a2[i] = ::atof(gasString.c_str());
+            b2[i] = ::atof(temperatureString.c_str());
+            c2[i] = ::atof(pressureString.c_str());
         }
 
         i++;
@@ -146,61 +145,73 @@ int main(void)
     printf("# OF DATA: %d \n", i - 1);
 	ip.close();
     
+    //Some values of real CO2 from 3000 to 3010
+    printf("REAL CO2 - A304= [");
+    for (int j=0; j<12; j++) printf(" %4.3f", a1[j]);
+    printf(" ...]\n");
+
+    //Some values of real CO2 from 3000 to 3010
+    printf("REAL CO2 - C114 = [");
+    for (int k=0; k<12; k++) printf(" %4.3f", a2[k]);
+    printf(" ...]\n");
+    
 
     //Timing
     struct timeval t1, t2;
     gettimeofday(&t1, 0);
 
-    for(int i=0;i < N;i++)
-	{   // loop over data in chunks
-	    // stream 1
-		cudaMemcpyAsync(d_a1, a1, size, cudaMemcpyHostToDevice, stream1);
-        cudaMemcpyAsync(d_b1, b1, size, cudaMemcpyHostToDevice, stream1);
-        cudaMemcpyAsync(d_c1, c1, size, cudaMemcpyHostToDevice, stream1);
-		getGasModelKernel1<<<1,N,0,stream1>>>(d_a1, d_b1, d_c1, d_d1);
-		cudaMemcpyAsync(d1, d_d1, size, cudaMemcpyDeviceToHost, stream1);
+    for(int i=0;i < N;i+= N*2) { // loop over data in chunks
 
-		//stream 2
-		cudaMemcpyAsync(d_a2, a2, size, cudaMemcpyHostToDevice, stream2);
-		cudaMemcpyAsync(d_b2, b2, size, cudaMemcpyHostToDevice, stream2);
-        cudaMemcpyAsync(d_c2, c2, size, cudaMemcpyHostToDevice, stream2);
-        getGasModelKernel2<<<1,N,1,stream2>>>(d_a2, d_b2, d_c2, d_d2);
-		cudaMemcpyAsync(d2, d_d2, size, cudaMemcpyDeviceToHost, stream2);
+        cudaMemcpyAsync(d_a1, a1, size, cudaMemcpyHostToDevice,stream1);
+        cudaMemcpyAsync(d_b1, b1, size, cudaMemcpyHostToDevice,stream1);
+        cudaMemcpyAsync(d_c1, c1, size, cudaMemcpyHostToDevice,stream1);
+
+        cudaMemcpyAsync(d_a2, a2, size, cudaMemcpyHostToDevice,stream2);
+        cudaMemcpyAsync(d_b2, b2, size, cudaMemcpyHostToDevice,stream2);
+        cudaMemcpyAsync(d_c2, c2, size, cudaMemcpyHostToDevice,stream2);
+
+        getGasModelKernel1<<<(int)ceil(N/1024)+1,1024,0,stream1>>>(d_a1, d_b1, d_c1, d_d1);
+        getGasModelKernel2<<<(int)ceil(N/1024)+1,1024,1,stream2>>>(d_a2, d_b2, d_c2, d_d2);
+            
+        //Copy from device to host
+        cudaMemcpyAsync(d1, d_d1, size, cudaMemcpyDeviceToHost,stream1);
+        cudaMemcpyAsync(d2, d_d2, size, cudaMemcpyDeviceToHost,stream2);
     }
-    
+
+
 	cudaStreamSynchronize(stream1); // wait for stream1 to finish
 	cudaStreamSynchronize(stream2); // wait for stream2 to finish
 
-    gettimeofday(&t2, 0);
-    float time = (1000000.0*(t2.tv_sec-t1.tv_sec) + t2.tv_usec-t1.tv_usec)/1000.0;
-    printf("EXECUTION TIME:  %5.4f ms \n", time);
 
     //A304
     //Some values of real CO2 from 3000 to 3010
     printf("MODEL CO2 - A304 = [");
-    for (int j=25000; j<25010; j++) printf(" %4.3f", d1[j]);
+    for (int j=0; j<12; j++) printf(" %4.3f", d1[j]);
     printf(" ...]\n");
 
-    //Some values of real CO2 from 3000 to 3010
-    printf("REAL CO2 - A304= [");
-    for (int j=25000; j<25010; j++) printf(" %4.3f", a1[j]);
-    printf(" ...]\n");
     
     
     //C114
     //Some values of real CO2 from 3000 to 3010
     printf("MODEL CO2 - C114 = [");
-    for (int j=25000; j<25010; j++) printf(" %4.3f", d2[j]);
+    for (int k=0; k<12; k++) printf(" %4.3f", d2[k]);
     printf(" ...]\n");
 
-    //Some values of real CO2 from 3000 to 3010
-    printf("REAL CO2 - C114 = [");
-    for (int j=25000; j<25010; j++) printf(" %4.3f", a2[j]);
-    printf(" ...]\n");
+    gettimeofday(&t2, 0);
+    double time = (1000000.0*(t2.tv_sec-t1.tv_sec) + t2.tv_usec-t1.tv_usec)/1000.0;
+    printf("EXECUTION TIME:  %5.4f ms \n", time);
+
+    
+
+    
 
 
     //Destroying stream used
     cudaStreamDestroy(stream1);
     cudaStreamDestroy(stream2);
+
+    cudaFree(d_d1);
+    cudaFree(d_d2);
+
 	return 0;
 } 
